fdfae6030f44aceb1e44a00694453578d1240eb2
Charlie Forsberg Hedberg <cfh91@hotmail.com>
2017-07-04 16:51:34 +0200

    Init

---
+++ Math/Vector2.ts
@@ -0,0 +1,15 @@
+export class Vector2 {
+    static Zero:  Vector2 = new Vector2(0, 0);
+    static One:   Vector2 = new Vector2(1, 1);
+    static UnitX: Vector2 = new Vector2(1, 0);
+    static UnitY: Vector2 = new Vector2(0, 1);
+
+    constructor(public X: number = 0, 
+                public Y: number = 0) {
+
+    }
+
+    public Negate(): Vector2 {
+        return new Vector2(-this.X, -this.Y);
+    }
+}
