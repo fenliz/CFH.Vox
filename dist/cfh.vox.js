f5f0d22575709dbfc8ad040aaba080a8c1824881
Charlie Forsberg Hedberg <cfh91@hotmail.com>
2017-07-05 11:35:28 +0200

    Commit build for GitHub Pages

---
+++ dist/cfh.vox.js
@@ -0,0 +1,146 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+var Game_1 = __webpack_require__(1);
+var game;
+window.onload = function () {
+    game = new Game_1.Game(1024, 800);
+    game.start();
+};
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+var Canvas_1 = __webpack_require__(2);
+var Game = (function () {
+    function Game(width, height) {
+        this.canvas = new Canvas_1.Canvas(width, height);
+    }
+    Game.prototype.start = function () {
+        this.isRunning = true;
+        this.processFrame();
+    };
+    Game.prototype.stop = function () {
+        this.isRunning = false;
+    };
+    Game.prototype.processFrame = function () {
+        if (this.isRunning) {
+            this.draw();
+            // Request a new frame from the browser and recursively call this method again.
+            requestAnimationFrame(this.processFrame.bind(this));
+        }
+    };
+    Game.prototype.draw = function () {
+        this.canvas.clear();
+    };
+    return Game;
+}());
+exports.Game = Game;
+
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+var Canvas = (function () {
+    function Canvas(width, height) {
+        this.canvas = document.getElementById("glCanvas");
+        this.canvas.width = width;
+        this.canvas.height = height;
+        this.gl = this.canvas.getContext("webgl", { preserveDrawingBuffer: true });
+        if (!this.gl) {
+            alert("Error getting the WebGL context. Please check if your browser supports it.");
+            return;
+        }
+        this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
+        this.gl.clearDepth(1.0);
+        this.gl.enable(this.gl.DEPTH_TEST);
+        this.gl.depthFunc(this.gl.LEQUAL);
+    }
+    Canvas.prototype.clear = function () {
+        this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
+    };
+    return Canvas;
+}());
+exports.Canvas = Canvas;
+
+
+/***/ })
+/******/ ]);
