c6a2465a1be9d0f66555cc22ab39d2e2bbf89eea
Charlie Forsberg Hedberg <cfh91@hotmail.com>
2017-07-05 16:08:55 +0200

    First implementation of HUD and FPS counter

--- src/engine/canvas.ts
+++ src/engine/canvas.ts
@@ -1,14 +1,30 @@
 
 export class Canvas {
-    public canvas: HTMLCanvasElement;
+    public glCanvas: HTMLCanvasElement;
+    public hudCanvas: HTMLCanvasElement;
+
+    public debugCanvas: HTMLCanvasElement;
     public gl: WebGLRenderingContext;
+    public hud: CanvasRenderingContext2D;
 
     constructor(width: number, height: number) {
-        this.canvas = document.getElementById("glCanvas") as HTMLCanvasElement;
-        this.canvas.width = width;
-        this.canvas.height = height;
+        this.glCanvas = document.getElementById("glCanvas") as HTMLCanvasElement;
+        this.hudCanvas = document.getElementById("hudCanvas") as HTMLCanvasElement;
 
-        this.gl = this.canvas.getContext("webgl", { preserveDrawingBuffer: true }) as WebGLRenderingContext;
+        this.hudCanvas.width = this.glCanvas.width = width;
+        this.hudCanvas.height = this.glCanvas.height = height;
+
+        this.initWebGL();
+        this.initHUD();
+    }
+
+    public clear(): void {
+        this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
+        this.hud.clearRect(0, 0, this.hudCanvas.width, this.hudCanvas.height);
+    }
+
+    private initWebGL(): void {
+        this.gl = this.glCanvas.getContext("webgl", { preserveDrawingBuffer: true }) as WebGLRenderingContext;
         if (!this.gl) {
             alert("Error getting the WebGL context. Please check if your browser supports it.");
             return;
@@ -20,7 +36,10 @@ export class Canvas {
         this.gl.depthFunc(this.gl.LEQUAL);
     }
 
-    public clear(): void {
-        this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
+    private initHUD(): void {
+        this.hud = this.hudCanvas.getContext("2d") as CanvasRenderingContext2D;
+        if (!this.hud) {
+            alert("Error getting the Canvas2D context. Please check if your browser supports it.");
+        }
     }
 }
--- src/engine/game.ts
+++ src/engine/game.ts
@@ -4,29 +4,63 @@ export class Game {
     public isRunning: boolean;
     public canvas: Canvas;
 
+    private timeAtLastFrame: number = new Date().getTime();
+    private idealTimePerFrame: number = 1000 / 60;
+    private leftover: number = 0;
+    private frames: number = 0;
+    private fpsReset: number = 0;
+    private fps: number = 0;
+
     constructor(width: number, height: number) {
         this.canvas = new Canvas(width, height);
     }
 
     public start(): void {
         this.isRunning = true;
-        this.processFrame();
+        this.tick();
     }
 
     public stop(): void {
         this.isRunning = false;
     }
 
-    private processFrame(): void {
+    private tick(): void {
         if (this.isRunning) {
+            const timeAtThisFrame: number = new Date().getTime();
+            const deltaTime: number = timeAtThisFrame - this.timeAtLastFrame;
+            const timeSinceLastTick: number = deltaTime + this.leftover;
+            const catchUpFrameCount: number = Math.floor(timeSinceLastTick / this.idealTimePerFrame);
+
+            this.fpsReset += deltaTime;
+            if (this.fpsReset > 1000) {
+                this.fps = this.frames - 1;
+                this.fpsReset = 0;
+                this.frames = 0;
+            }
+
+            for (let i: number = 0; i < catchUpFrameCount; i++) {
+                this.update();
+                this.frames++;
+            }
+
             this.draw();
 
+            this.leftover = timeSinceLastTick - (catchUpFrameCount * this.idealTimePerFrame);
+            this.timeAtLastFrame = timeAtThisFrame;
+
             // Request a new frame from the browser and recursively call this method again.
-            requestAnimationFrame(this.processFrame.bind(this));
+            requestAnimationFrame(this.tick.bind(this));
         }
+    }
+
+    private update(): void {
+        return;
     }
 
     private draw(): void {
         this.canvas.clear();
+
+        this.canvas.hud.fillStyle = "white";
+        this.canvas.hud.fillText("" + this.fps, 5, 15);
     }
 }
